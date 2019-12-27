8e1f7b8ad0f1b1699854cf910016ae7c17aeea7c
Felix Schildmann <felixlennart@t-online.de>
2019-12-27 13:32:08 +0100

    added sleep between doubleclick clicks

--- src/main/java/de/felix/webtv/RaspberryPiConnector.java
+++ src/main/java/de/felix/webtv/RaspberryPiConnector.java
@@ -32,9 +32,10 @@ public class RaspberryPiConnector {
         // Deal with different player types
         if(senderUrl.contains("ustvgo")){
             Robot r = new Robot();
-            r.mouseMove(50, 50);
+            r.mouseMove(80, 80);
             r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
             r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            Thread.sleep(200);
             r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
             r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
         }else {
